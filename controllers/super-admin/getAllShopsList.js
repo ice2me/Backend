cea1040440fe858442eec0b60a682eb38953932f
ice2me <ice2me1989@gmail.com>
2023-02-13 12:24:52 +0200

    add super admin update shop

--- controllers/super-admin/getAllShopsList.js
+++ controllers/super-admin/getAllShopsList.js
@@ -9,3 +9,40 @@ export const getAllShopsList = async (req,
 		res.json({message: 'Something went wrong.'})
 	}
 }
+export const postUpdateShop = async (req,
+	res) => {
+	const {
+		id,
+		shop_name,
+		email,
+		phone,
+		variant_trading,
+		date_payment,
+		end_date_paid_period,
+		paid_subscription,
+		pass
+	} = req.body
+	try {
+		if (pass === process.env.PASS_FOR_UPDATE) {
+			const isShop = await User.findById(id)
+
+			isShop.shop_name = shop_name
+			isShop.email = email
+			isShop.phone = phone
+			isShop.variant_trading = variant_trading
+			isShop.date_payment = date_payment
+			isShop.end_date_paid_period = end_date_paid_period
+			isShop.paid_subscription = paid_subscription
+
+			await isShop.save()
+
+			res.json({shops: isShop})
+		}else {
+			res.json({message: 'Non Password'})
+		}
+
+	} catch (e) {
+		res.json({message: 'Something went wrong.'})
+	}
+}
+
--- routes/superAdmin.js
+++ routes/superAdmin.js
@@ -1,9 +1,13 @@
 import { Router } from "express"
-import { getAllShopsList } from "../controllers/super-admin/getAllShopsList.js"
-import { checkAuth } from "../utils/checkAuth.js";
+import {
+	getAllShopsList,
+	postUpdateShop
+} from "../controllers/super-admin/getAllShopsList.js"
+import { checkAuth } from "../utils/checkAuth.js"
 
 const router = new Router()
 
 router.get('/list', checkAuth, getAllShopsList)
+router.post('/update-shop', checkAuth, postUpdateShop)
 
 export default router
