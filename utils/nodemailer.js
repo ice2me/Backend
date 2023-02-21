d5b9777f6da2e46ddf392a0c7821f0fd33385a9d
ice2me <ice2me1989@gmail.com>
2023-02-21 13:43:12 +0200

    update leater

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -30,63 +30,57 @@ export const nodeMailer = (
 			subject: "Order",
 			text: "Order client",
 			html: `
-				<h1>Order for ${shop_name}</h1>
-				<h3>Client contact <br>
-					Name: ${username} <br>
-					Phone: ${phone} <br>
-					Email: ${user_email} <br>
-					City: ${city} <br>
-					Address: ${address} <br>
+				<h1>Order for / Замовлення для <span style="background-color: rgba(247,206,0, 0.3)">${shop_name}</span>
+				</h1>
+				<h3>Client contact / Контакти клієнта <br>
+					Name / Імь'я: <span style="background-color: rgba(247,206,0, 0.3)">${username}</span> <br>
+					Phone / Телефон: <span style="background-color: rgba(247,206,0, 0.3)">${phone}</span> <br>
+					Email / Електронна адреса: <span style="background-color: rgba(247,206,0, 0.3)">${user_email}</span> <br>
+					City / Місто: <span style="background-color: rgba(247,206,0, 0.3)">${city}</span> <br>
+					Address / Адреса: <span style="background-color: rgba(247,206,0, 0.3)">${address}</span> <br>
 				</h3>
 				<p style='width: 300px;'>
-					Comment Message: ${comment_message} <br>
+					Comment Message / Коментар: <span style="background-color: rgba(247,206,0, 0.5);">${comment_message}</span> <br>
 				</p>
 				<table style="font-size: 14px;">
 					${items?.map(item => (
 				`
 									<tr>
-										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: coral'>
-											Name product: 
+										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: rgba(255,127,80, 0.3)'>
+											Name product / Назва товару:
 										</th>
-										<td style='width: 250px; padding: 0 10px; background-color: limegreen'>
+										<td style='width: 250px; padding: 0 10px; background-color: rgba(50,205,50, 0.3)'>
 											${item?.name_product}
 										</td>
 									</tr>
 									<tr>
-										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: coral'>
-											Price: 
+										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: rgba(255,127,80, 0.3)'>
+											Price / Ціна:
 										</th>
-										<td style='width: 250px; padding: 0 10px; background-color: limegreen'>
+										<td style='width: 250px; padding: 0 10px; background-color: rgba(50,205,50, 0.3)'>
 											 ${item?.price_product} ${item?.currency_product}
 										</td>
 									</tr>
 									<tr>
-										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: coral'>
-											Unit product:  
+										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: rgba(255,127,80, 0.3)'>
+											Unit product / Кількість продукту: 
 										</th>
-										<td style='width: 250px; padding: 0 10px; background-color: limegreen'>
-											${item?.count}${item?.unit_product === 'gram' ||
-												item?.unit_product === 'грам' ||
-												item?.unit_product === 'milliliter' ||
-												item?.unit_product === 'мілілітр'
-													? '00'
-													: ''
-												} 
-												${item?.unit_product}
+										<td style='width: 250px; padding: 0 10px; background-color: rgba(50,205,50, 0.3)'>
+											${item?.count} ${item?.unit_product}
 										</td>
 									</tr>
 									<tr>
-										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: coral'>
-											Total Price:  
+										<th style='width: 100px; white-space: nowrap; padding: 0 10px; text-align: left; background-color: rgba(255,127,80, 0.3)'>
+											Total Price / Загальна сума за продукт:
 										</th>
-										<td style='width: 250px; padding: 0 10px; background-color: limegreen'>
+										<td style='width: 250px; padding: 0 10px; background-color: rgba(50,205,50, 0.3)'>
 											${item?.total_price} ${item?.currency_product}
 										</td>
 									</tr>
 								`
 			))}
 				</table>
-				<h3>Total Amount: ${totalAmount}₴</h3>
+				<h3>Total Amount / Загальна сума замовлення: <span style="background-color: rgba(247,206,0, 0.5); font-size: 22px">${totalAmount}₴</span></h3>
 				`
 		})
 
